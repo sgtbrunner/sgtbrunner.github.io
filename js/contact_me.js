3d6c23deb71c86d9b7184161564a8076f9f84d01
sgtbrunner <guilherme.brunner@gmail.com>
2019-06-25 16:06:36 -0300

    adding contact and fixing modals and footer

--- js/contact_me.js
+++ js/contact_me.js
@@ -20,11 +20,10 @@ $(function() {
       $this = $("#sendMessageButton");
       $this.prop("disabled", true); // Disable submit button until AJAX call is complete to prevent duplicate messages
       $.ajax({
-        url: "././mail/contact_me.php",
+        url: "https://formspree.io/guilherme.brunner@gmail.com",
         type: "POST",
         data: {
           name: name,
-          phone: phone,
           email: email,
           message: message
         },
